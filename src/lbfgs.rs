c3cdb0348a31900d432053596c599a0426d859d6
noshu <naushad1290@gmail.com>
2016-05-19 02:22:16 +0600

    some little bug fix

--- src/lbfgs.rs
+++ src/lbfgs.rs
@@ -4,6 +4,6 @@ use libc::{c_double,c_int,c_char};
 extern "C"{
     pub fn setulb_(n:*const c_int,m:*const c_int,x:*mut c_double,l:*const c_double,
     u:*const c_double,nbd:*const c_int,f:*mut c_double,g:*mut c_double,factr:*const c_double,
-    pgtol:*const c_double,wa:*mut c_double,iwa:*mut c_int,iprint:*const c_int,csave:*mut c_char,
+    pgtol:*const c_double,wa:*mut c_double,iwa:*mut c_int,task:*mut c_char,iprint:*const c_int,csave:*mut c_char,
     lsave:*mut c_int,isave:*mut c_int,dsave:*mut c_double);
 }
