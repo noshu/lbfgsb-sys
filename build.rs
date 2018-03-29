c487998258e011f4682a342869d98c633e7a276d
Félix C. Morency <felix.morency@imeka.ca>
2018-03-29 15:37:11 -0400

    Link statically with libgfortran and libquadmath
    Affects macOS and Windows only (cross-compile)

--- build.rs
+++ build.rs
@@ -19,18 +19,34 @@ fn main() {
     let os = if cfg!(target_os = "macos"){"Macos"}
              else if cfg!(target_os = "windows"){"Windows"}
              else {"Linux"};
-    let com = if os=="Windows" {"mingw32-make"}else{"make"};
-    run(Command::new(com)
+    run(Command::new("make")
                 .arg(kind)
-                .arg(format!("OUTPUT={}",output.display()))
-                .arg(format!("OSNAME={}",os))
+                .arg(format!("OUTPUT={}", output.display()))
+                .arg(format!("OSNAME={}", os))
                 .current_dir(&source));
 
     println!("cargo:rustc-link-search={}", output.display());
-    println!("cargo:rustc-link-lib={}=lbfgs",kind);
-    println!("cargo:rustc-link-lib=dylib=gfortran");
+    println!("cargo:rustc-link-lib={}=lbfgs", kind);
     println!("cargo:rustc-link-lib=dylib=gcc");
-    println!("cargo:rustc-link-lib=dylib=quadmath");
+
+    let target = variable!("TARGET");
+    let mut fc_lib_type = "dylib";
+    if target == "x86_64-apple-darwin" || target == "x86_64-pc-windows-gnu" {
+        fc_lib_type = "static";
+
+        // Poke $FC$ for static lib folder
+        let fc_out = Command::new(variable!("FC"))
+                              .arg("-print-file-name=libgfortran.a")
+                              .output()
+                              .expect("Failed to find libgfortran.a");
+        let fc_stdout = String::from_utf8_lossy(&fc_out.stdout);
+        let fc_lib_cwd = PathBuf::from(fc_stdout.to_string());
+        let fc_lib_pwd = fc_lib_cwd.parent().expect("Path to libgfortran.a not found");
+        println!("cargo:rustc-link-search={}", fc_lib_pwd.to_str().unwrap());
+    }
+
+    println!("cargo:rustc-link-lib={}=gfortran", fc_lib_type);
+    println!("cargo:rustc-link-lib={}=quadmath", fc_lib_type);
 }
 fn run(command: &mut Command) {
     println!("Running: {:?}", command);
